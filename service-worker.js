16c79ff18856f5957bacbd039267c27eae45ac3e
Miguel <miguel.meki@gmail.com>
2026-02-15 12:29:57 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'respira-v1.3.0-winter-14'; // ← Nueva versión
+const CACHE_NAME = 'respira-v1.3.0-winter-15'; // ← Nueva versión
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -49,6 +49,7 @@ self.addEventListener('fetch', (e) => {
   );
 
 });
+
 
 
 
